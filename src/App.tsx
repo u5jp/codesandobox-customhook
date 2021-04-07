9bd54402ebd80688c51a58294553769120e4d083
u5jp <65936222+u5jp@users.noreply.github.com>
2021-04-07 12:20:20 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
+
+const user = {
+  id: 1,
+  name: "太郎",
+  email: "123@gmail",
+  address: "Address"
+};
+
+export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+      <UserCard user={user} />
+    </div>
+  );
+}
